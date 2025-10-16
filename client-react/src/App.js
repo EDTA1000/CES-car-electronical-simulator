3f272aa8d33ba45badfb8f47104fc7377f9415a6
Danial Alinasiri <your@email.com>
2025-10-16 12:31:37 +0330

    رفع خطای JSX و بیلد جدید

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 function App() {
-<div className="ces-container">
-  <img src="/CES.png" alt="CES Logo" className="ces-logo" />
-  <img src="/wheel.png" alt="Wheel" className="wheel" />
-  <img src="/lighting.png" alt="Lightning" className="lightning" />
-</div>
-<button className="subscribe-button" onClick={() => window.location.href = "/subscribe"}>
-  اشتراک
-</button>
+  return (
+    <div className="App">
+      <div className="ces-container">
+        <img src="/CES.png" alt="CES Logo" className="ces-logo" />
+        <img src="/wheel.png" alt="Wheel" className="wheel" />
+        <img src="/lighting.png" alt="Lightning" className="lightning" />
+      </div>
+      <button className="subscribe-button" onClick={() => window.location.href = "/subscribe"}>
+        اشتراک
+      </button>
+    </div>
+  );
 }
 export default App;
