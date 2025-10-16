12cfe1a172ddf4c9f9d2f184cfbae80ce9503597
Danial Alinasiri <your@email.com>
2025-10-16 11:51:29 +0330

    ...

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="logo-container">
+        <img src="/logo512.png" alt="logo512" className="logo512" />
+        <div className="lightning"></div>
+        <div className="lightning"></div>
+        <div className="lightning"></div>
+      </div>
     </div>
   );
 }
-
+<button className="subscribe-button" onClick={() => window.location.href = "/subscribe"}>
+  اشتراک
+</button>
 export default App;
